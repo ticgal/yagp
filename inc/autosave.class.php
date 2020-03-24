9691d6d719bb6e4c9f465a7e9d68c3097f517e81
Xacobo Freire <xacobo@ticgal.com>
2020-03-24 11:29:52 +0100

    fix:multiple forms

--- inc/autosave.class.php
+++ inc/autosave.class.php
@@ -48,50 +48,63 @@ class PluginYagpAutosave extends CommonDBTM {
 		$script=<<<JAVASCRIPT
 			$(document).ready(function(){
 				if(typeof(Storage) !== "undefined") {
-					if (localStorage.getItem("{$itemtype}") !== null) {
-						var content=localStorage.getItem("{$itemtype}");
-						$('<div>'+content+'</div>').appendTo('body').dialog({
-							modal: true,
-							title: 'Load draft?',
-							autoOpen: true,
-							width: '1050px',
-							resizable: false,
-							buttons: {
-								Yes: function() {
-									var iframe=$("div.mce-tinymce iframe").contents();
-									$(iframe[0].body).html(content);
-									$(this).dialog("close");
+					var iframe="";
+					var id="";
+					var html="<p><br data-mce-bogus='1'></p>";
+					var interval=setInterval(function () {
+						iframe=$("div.mce-tinymce iframe");
+						console.log(iframe);
+						if (iframe[0].length!==0) {
+							id=$(iframe).attr('id');
+							iframe=$("iframe#"+id).contents();
+							clearInterval(interval);
+							loaddraft();
+						}
+					},1000);
+					
+					var loaddraft=function(){
+						if (localStorage.getItem("{$itemtype}") !== null) {
+							var content=localStorage.getItem("{$itemtype}");
+							$('<div>'+content+'</div>').appendTo('body').dialog({
+								modal: true,
+								title: 'Load draft?',
+								autoOpen: true,
+								width: '1050px',
+								resizable: false,
+								buttons: {
+									Yes: function() {
+										$(iframe[0].body).html(content);
+										$(this).dialog("close");
+									},
+									No: function() {
+										$(this).dialog("close");
+									}
 								},
-								No: function() {
-									$(this).dialog("close");
+								close: function(event, ui) {
+									localStorage.removeItem("{$itemtype}");
+									$(this).remove();
+								},
+								open: function(event, ui){
+									$("div.ui-dialog-buttonset").css({"width":"50%","margin": "0 auto","float":"initial"});
+									$(".ui-dialog-buttonpane button:first").css({"float":"left"});
+									$(".ui-dialog-buttonpane button:last").css({"float":"right"});
 								}
-							},
-							close: function(event, ui) {
-								localStorage.removeItem("{$itemtype}");
-								$(this).remove();
-							},
-							open: function(event, ui){
-								$("div.ui-dialog-buttonset").css({"width":"50%","margin": "0 auto","float":"initial"});
-								$(".ui-dialog-buttonpane button:first").css({"float":"left"});
-								$(".ui-dialog-buttonpane button:last").css({"float":"right"});
-							}
-						});
+							});
 
-						setInterval(function () {
-							var iframe=$("div.mce-tinymce iframe").contents();
-							var html=$(iframe[0].body).html();
-							if ($(html).find("br[data-mce-bogus]").length===0) {
-								localStorage.setItem("{$itemtype}", $(iframe[0].body).html());
-							}
-						},5000);
-					}else{
-						setInterval(function () {
-							var iframe=$("div.mce-tinymce iframe").contents();
-							var html=$(iframe[0].body).html();
-							if ($(html).find("br[data-mce-bogus]").length===0) {
-								localStorage.setItem("{$itemtype}",$(iframe[0].body).html());
-							}
-						},5000);
+							setInterval(function () {
+								html=$(iframe[0].body).html();
+								if ($(html).find("br[data-mce-bogus]").length===0) {
+									localStorage.setItem("{$itemtype}", $(iframe[0].body).html());
+								}
+							},5000);
+						}else{
+							setInterval(function () {
+								html=$(iframe[0].body).html();
+								if ($(html).find("br[data-mce-bogus]").length===0) {
+									localStorage.setItem("{$itemtype}",$(iframe[0].body).html());
+								}
+							},5000);
+						}
 					}
 				}
 			});
