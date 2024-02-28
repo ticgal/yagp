5f2397ac37d22773dc2283b5c21d0250dd063f9d
FrancisTicgal <francisco@ticgal.com>
2024-02-28 08:47:51 +0100

    fix blockdate option

--- js/blockdate.js
+++ js/blockdate.js
@@ -4,17 +4,15 @@
 $(document).ready(function() {
     if (window.location.href.indexOf("/front/ticket.form.php?id=") > -1) {
         function blockDateField() {
-            console.log('Blocking date field');
+            var input = $('div#itil-data input[name="date"]').next();
+            input.attr('disabled', 'disabled');
         }
 
-        blockDateField();
-        /*
         var interval = setInterval(() => {
-            if ($('input[name="date"]').length > 0) {
+            if ($('div#itil-data input[name="date"]').length > 0) {
                 clearInterval(interval);
                 blockDateField();
             }
         }, 100);
-        */
     }
 });
