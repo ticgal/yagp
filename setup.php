e001d77e1f17e680c25b2e0a65ed096eec0f5c2c
Aarón <aaron@ticgal.com>
2026-01-16 10:28:23 +0100

    update version

--- setup.php
+++ setup.php
@@ -31,7 +31,7 @@
 
 use Glpi\Plugin\Hooks;
 
-define('PLUGIN_YAGP_VERSION', '3.0.0-beta.2');
+define('PLUGIN_YAGP_VERSION', '3.0.0-beta.3');
 // Minimal GLPI version, inclusive
 define("PLUGIN_YAGP_MIN_GLPI", "11.0");
 // Maximum GLPI version, exclusive
